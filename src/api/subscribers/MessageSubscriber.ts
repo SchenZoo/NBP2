649fa85d9bcc434229593a25c8eb23918e68e7af
Aleksandar Stankovic <stankovic.aleksandar@elfak.rs>
2020-05-10 12:20:26 +0200

    fix(web push): remove web push notifications

--- src/api/subscribers/MessageSubscriber.ts
+++ src/api/subscribers/MessageSubscriber.ts
@@ -1,21 +1,17 @@
 import { EventSubscriber, On } from 'event-dispatch'
 import Container from 'typedi'
 import { SocketService } from '../services/SocketService'
-import { WebPushNotificationService } from '../services/WebPushNotificationService'
 import { IPrivateMessageSent } from '../app_models/event_dispatch/IPrivateMessageSent'
 import { ChatSessionModel } from '../database/models/ChatSession'
 import { SocketEventNames } from '../../constants/SocketEventNames'
 import { getUserRoom } from '../../constants/SocketRoomNames'
-import { getChatLink } from '../misc/Links'
 import { MessageModel, IMessage } from '../database/models/Message'
 
 @EventSubscriber()
 export class MessageSubscriber {
   private socketService: SocketService
-  private webPushService: WebPushNotificationService
   constructor() {
     this.socketService = Container.get(SocketService)
-    this.webPushService = Container.get(WebPushNotificationService)
   }
 
   @On('privateMessageSent')
@@ -39,12 +35,5 @@ export class MessageSubscriber {
         )
       })
     })()
-    const payload = this.webPushService.makeWebPushNotification(
-      body.sender.username + ' ti je poslao poruku',
-      body.message.text ? body.message.text : 'Pogledaj u aplikaciji :)',
-      'chat-user-' + body.sender.id,
-      getChatLink(body.sender.id),
-    )
-    this.webPushService.sendWebPushNotification(payload, body.receiverId)
   }
 }
--- src/api/subscribers/NotificationSubscriber.ts
+++ src/api/subscribers/NotificationSubscriber.ts
@@ -1,6 +1,5 @@
 import { EventSubscriber, On } from 'event-dispatch'
 import { SocketService } from '../services/SocketService'
-import { WebPushNotificationService } from '../services/WebPushNotificationService'
 import Container from 'typedi'
 import { INotificationSent } from '../app_models/event_dispatch/INotificationSent'
 import { SocketEventNames } from '../../constants/SocketEventNames'
@@ -10,26 +9,14 @@ import { EmailService } from '../services/EmailService'
 @EventSubscriber()
 export class NotificationSubscriber {
   private socketService: SocketService
-  private webPushService: WebPushNotificationService
   private emailService: EmailService
   constructor() {
     this.socketService = Container.get(SocketService)
-    this.webPushService = Container.get(WebPushNotificationService)
     this.emailService = Container.get(EmailService)
   }
 
   @On('notificationSent')
   async onUserCreate(body: INotificationSent) {
-    // SENDING VIA WEB PUSH
-    const payload = this.webPushService.makeWebPushNotification(
-      'Nova notifikacija od ' + body.userFrom.username,
-      body.notification.text,
-      'notification-' + body.receiverId,
-      process.env.APP_URL + '/' + body.notification.relativeLink,
-    )
-
-    this.webPushService.sendWebPushNotification(payload, body.receiverId)
-    // SENDING VIA SOCKET
     this.socketService.sendEventInRoom(SocketEventNames.NOTIFICATION, body.notification, getUserRoom(body.receiverId))
     // SENDING EMAIL
     this.emailService.sendEmail(body.receiverId, 'Nova notifikacija od ' + body.userFrom.username, body.notification.text)
