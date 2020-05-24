f60d5ad9a3bcc211fb24bbe6740fa6af26b21e7a
Aleksandar Stankovic <stankovic.aleksandar@elfak.rs>
2020-05-24 18:03:36 +0200

    fix(urls)

--- src/constants/ModelImagePath.ts
+++ src/constants/ModelImagePath.ts
@@ -1,15 +1,21 @@
-import path = require('path');
-import { API_PATH } from '../api/configurations/paths';
+import path = require("path");
+import { API_PATH } from "../api/configurations/paths";
 
 export enum ModelImagePath {
-  SECTION = 'section',
-  USER_PROFILE = 'user/profile',
-  PRIVATE_CHAT_MESSAGE = 'chat/private',
-  COMMENT = 'comment',
+  SECTION = "section",
+  USER_PROFILE = "user/profile",
+  PRIVATE_CHAT_MESSAGE = "chat/private",
+  COMMENT = "comment",
 }
 
-export const DEFAULT_IMAGE_PATH = 'public/images/';
+export const DEFAULT_IMAGE_PATH = "public/images/";
 
-export const getModelImageUrl = imageName => imageName ? `${API_PATH}/${DEFAULT_IMAGE_PATH}${imageName}` : null;
+export const getModelImageUrl = (imageName) =>
+  imageName
+    ? imageName.includes(`${DEFAULT_IMAGE_PATH}`)
+      ? imageName
+      : `${API_PATH}/${DEFAULT_IMAGE_PATH}${imageName}`
+    : null;
 
-export const getAbsoluteServerPath = imageName => path.resolve(DEFAULT_IMAGE_PATH + imageName);
+export const getAbsoluteServerPath = (imageName) =>
+  path.resolve(DEFAULT_IMAGE_PATH + imageName);
