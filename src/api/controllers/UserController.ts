859a1324403fe6449277ba4ae3ee24701c6cb230
Aleksandar Stankovic <aleksandar@ncoded.solutions>
2021-04-07 17:28:40 +0200

    feat(user routes): Get/delete participants routes

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -27,12 +27,15 @@ import {
   UseBefore,
   Req,
   QueryParams,
+  Delete,
+  Post,
 } from "routing-controllers";
 import { IUser, UserModel } from "../database/models/User";
 import { UserValidator } from "../validators/UserValidator";
 import { hashPassowrd } from "../misc/Hash";
 import { UserPolicy } from "../policy/UserPolicy";
 import { PaginationSearch } from "../misc/QueryPagination";
+import { ObjectFromParamNotFound } from "../errors/ObjectFromParamNotFound";
 
 @JsonController("/users")
 export class UserController {
@@ -40,6 +43,21 @@ export class UserController {
     private fileService: FileService,
     private friendRepo: FriendRepository
   ) {}
+
+  @Get("/participants")
+  @UseBefore(checkRole([RoleNames.ADMIN, RoleNames.PROFESSOR]))
+  @UseBefore(passportJwtMiddleware)
+  public async getParticipantsPaginated(
+    @QueryParams() query: PaginationSearch
+  ) {
+    return UserModel.find({
+      username: { $regex: `${query.name ? query.name : ""}` },
+      roles: RoleNames.PARTICIPANT,
+    })
+      .sort({ createdAt: -1 })
+      .paginate(query.skip, query.take)
+      .cache();
+  }
 
   @Get("/")
   @UseBefore(checkRole([RoleNames.ADMIN, RoleNames.PROFESSOR]))
@@ -53,7 +71,7 @@ export class UserController {
       .cache();
   }
 
-  @Get("/:id")
+  @Get("/:id([a-z0-9]{24})")
   @UseBefore(passportJwtAnonymousMiddleware)
   public async get(@Param("id") id: string, @CurrentUser() user?: IUser) {
     const [gettingUser, friendRequest, friendship] = await Promise.all([
@@ -69,25 +87,55 @@ export class UserController {
     return { user: gettingUser, friendRequest, friendship };
   }
 
+  @Post()
+  @UseBefore(checkRole([RoleNames.ADMIN]))
+  @UseBefore(passportJwtMiddleware)
+  public async create(
+    @Body({ validate: { whitelist: true } }) participant: UserValidator
+  ) {
+    participant.roles = [RoleNames.PARTICIPANT];
+    participant.password = hashPassowrd(participant.password);
+    if (participant.imageBase64) {
+      participant.imageURL = await this.fileService.addBase64Image(
+        participant.imageBase64,
+        ModelImagePath.USER_PROFILE
+      );
+    }
+
+    return new UserModel(participant).save();
+  }
+
   @Put("/:id")
   @UseBefore(policyCheck(BASE_POLICY_NAMES.UPDATE, UserPolicy))
   @UseBefore(passportJwtMiddleware)
   public async update(
     @Body({ validate: { skipMissingProperties: true, whitelist: true } })
     updatedUser: UserValidator,
-    @Param("id") id: string,
-    @Req() request: IUserPolictyRequest
+    @Param("id") id: string
   ) {
     if (updatedUser.password) {
       updatedUser.password = hashPassowrd(updatedUser.password);
     }
     if (updatedUser.imageBase64) {
-      const user = request.requestUser;
       updatedUser.imageURL = await this.fileService.addBase64Image(
         updatedUser.imageBase64,
         ModelImagePath.USER_PROFILE
       );
     }
     return UserModel.findByIdAndUpdate(id, updatedUser, { new: true });
+  }
+
+  @Delete("/:id")
+  @UseBefore(policyCheck(BASE_POLICY_NAMES.DELETE, UserPolicy))
+  @UseBefore(passportJwtMiddleware)
+  public async delete(
+    @Req() request: IUserPolictyRequest,
+    @Param("id") id: string
+  ) {
+    const user = request.requestUser;
+    if (!user || user.hasRoles([RoleNames.ADMIN, RoleNames.PROFESSOR])) {
+      throw new ObjectFromParamNotFound("User", id);
+    }
+    return user.remove();
   }
 }
